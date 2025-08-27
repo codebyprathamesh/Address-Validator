4365cb43b9abbea474dea145cc12f007e2d6232f
codebyprathamesh <prathameshmore104@gmail.com>
2025-08-27 19:09:31 +0530

    Create AddressValidator.py

---
+++ AddressValidator.py
@@ -0,0 +1,9 @@
+def email(address):
+  dot=address.find(".")
+  at=address.find("@")
+      while True:
+    if (dot==-1):
+      print("Invalid Email Address")
+    elif (at==-1):
+      print("Invalid Email Address")
+    else print("This is a valid email address")
