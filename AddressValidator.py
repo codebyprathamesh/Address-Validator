bb87c6b3cfcf9bc53b3e4dc8b91f01594e6407a3
codebyprathamesh <prathameshmore104@gmail.com>
2025-08-27 19:29:27 +0530

    added new change in code(prevented code from infinite execution)

--- AddressValidator.py
+++ AddressValidator.py
@@ -1,9 +1,13 @@
 def email(address):
   dot=address.find(".")
   at=address.find("@")
-      while True:
+  while True:
     if (dot==-1):
       print("Invalid Email Address")
     elif (at==-1):
       print("Invalid Email Address")
-    else print("This is a valid email address")
+    else:
+      print("This is a valid email address")
+      break
+email("prathameshmore792gmail.com")
+
